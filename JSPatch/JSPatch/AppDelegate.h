126b32322f090fb0903ff375b2061420353fcf8d
赵鹤 <megahertzchn@gmail.com>
2022-08-30 15:28:10 +0800

    [JSPatch] - 项目初始化

---
+++ JSPatch/JSPatch/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JSPatch
+//
+//  Created by 赵鹤 on 2022/8/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JSPatch/JSPatch/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  JSPatch
+//
+//  Created by 赵鹤 on 2022/8/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
